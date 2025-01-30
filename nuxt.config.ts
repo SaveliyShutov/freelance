95a5dc88c9564ca5fbbacf02b6fda2cd4a10679e
SaveliyShutov <savelijsutov@gmail.com>
2025-01-30 14:20:44 +0500

    add vuetify and pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+      }
+    }],
+    "@pinia/nuxt",
+  ],
+
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+  },
+
+  compatibilityDate: '2025-01-30'
 })
