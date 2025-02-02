069f72b1f56154c8782e27f75174d73a4bb99593
SaveliyShutov <savelijsutov@gmail.com>
2025-02-02 17:07:02 +0500

    add sign up

--- pages/sign.vue
+++ pages/sign.vue
@@ -1,21 +1,21 @@
 <template>
-  <v-container class="min-h-screen flex items-center justify-center py-12">
+  <v-container class="min-h-screen flex  justify-center py-12">
     <v-row justify="center">
       <v-col cols="12" sm="8" md="6" lg="4">
         <div class="space-y-8">
           <div>
-            <h2 class="text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+            <h2 class="text-center text-3xl font-extrabold text-gray-900">{{ isSignUp ? 'Войти' : 'Создать аккаунт' }}</h2>
             <p class="mt-2 text-center text-sm text-gray-600">
-              Or
+              или
               <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500" @click.prevent="isSignUp = !isSignUp">
-                {{ isSignUp ? 'sign in to your account' : 'create a new account' }}
+                {{ isSignUp ? 'создать новый акканут' : 'войти в акканут' }}
               </a>
             </p>
           </div>
           <form class="space-y-6" @submit.prevent="handleSubmit">
             <div class="rounded-md shadow-sm -space-y-px">
               <div>
-                <label for="email-address" class="sr-only">Email address</label>
+                <label for="email-address" class="sr-only">Электроная почта</label>
                 <input
                   id="email-address"
                   name="email"
@@ -23,11 +23,23 @@
                   required
                   v-model="email"
                   class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+                  placeholder="Электроная почта"
+                />
+              </div>
+              <div v-if="!isSignUp">
+                <label for="phone" class="sr-only">Телефон</label>
+                <input
+                  id="phone"
+                  name="phone"
+                  type="phone"
+                  required
+                  v-model="phone"
+                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Телефон"
                 />
               </div>
               <div>
-                <label for="password" class="sr-only">Password</label>
+                <label for="password" class="sr-only">Пароль</label>
                 <input
                   id="password"
                   name="password"
@@ -35,7 +47,7 @@
                   required
                   v-model="password"
                   class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
+                  placeholder="Пароль"
                 />
               </div>
             </div>
@@ -45,7 +57,7 @@
                 type="submit"
                 class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                {{ isSignUp ? 'Sign up' : 'Sign in' }}
+                {{ isSignUp ? 'Войти' : 'Зарегистрироваться' }}
               </button>
             </div>
           </form>
