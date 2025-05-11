140aaffebd8a97d53a02babe3a0485c8e420159e
SaveliyShutov <savelijsutov@gmail.com>
2025-05-12 01:03:46 +0500

    add favicons to nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
+  site: {
+    url: 'https://nirby.ru',
+    name: 'Nirby - работа рядом'
+  },
   devtools: { enabled: false },
 
   build: {
@@ -20,6 +24,7 @@ export default defineNuxtConfig({
       }
     }],
     "@pinia/nuxt",
+    '@nuxtjs/sitemap',
   ],
 
   runtimeConfig: {
@@ -57,6 +62,17 @@ export default defineNuxtConfig({
 
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
   },
 
   compatibilityDate: '2025-01-30'
